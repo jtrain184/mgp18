453dce2dc8dec4e4c3aa493cde8730e6971d6387
James Cameron Abreu <coolcam222@gmail.com>
2018-06-25 23:06:16 -0600

    Began work on gameStateManager and State classes

---
+++ Monogame Party 2018/Monogame Party 2018/GameStateManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Monogame_Party_2018 {
+
+  public class GameStateManager {
+
+
+    // List of states, we will loop over these:
+    List<State> states = new List<State>();
+    List<State> statesToUpdate = new List<State>();
+
+    SpriteBatch spriteBatch;
+    SpriteFont font;
+    Texture2D blankTexture;
+
+    KeyboardState input;
+
+
+
+    // Default spritebatch shared by all the states
+    public SpriteBatch SpriteBatch {
+      get { return spriteBatch; }
+    }
+
+    // font shared by all the states:
+    public SpriteFont Font {
+      get { return font; }
+    }
+
+
+    public void Update(GameTime gameTime) {
+
+      // TODO
+      // Read keyboard:
+      input = Keyboard.GetState();
+
+      // Make a copy of the current state list to avoid confusion
+      // if one state removes other states, etc...
+      statesToUpdate.Clear();
+      foreach (State s in states)
+        statesToUpdate.Add(s);
+
+
+      // Loop through all states and update:
+      while (statesToUpdate.Count > 0) {
+
+        // pop topmost state:
+        State s = statesToUpdate[statesToUpdate.Count - 1];
+        statesToUpdate.RemoveAt(statesToUpdate.Count - 1);
+
+        // Update:
+        if (s.active) {
+          s.Update(gameTime);
+        }
+        
+      } // end while
+    } // end UPDATE
+
+
+    public void Draw(GameTime gameTime) {
+      foreach (State s in states) {
+
+        // If visible, draw:
+        if (s.visible) {
+          s.Draw(gameTime);
+        }
+      } // end foreach
+    } // end DRAW
+
+
+
+    // TODO
+    public void AddState(State state, int playerIndex) {
+      states.Add(state);
+    }
+
+    // TODO
+    public void RemoveState(State state) {
+      states.Remove(state);
+      statesToUpdate.Remove(state);
+    }
+
+
+
+
+
+  } // end class definition
+} // end namespace
---
+++ Monogame Party 2018/Monogame Party 2018/State.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_Party_2018 {
+
+  public abstract class State {
+
+    public bool active;
+    public bool visible;
+    public int depth;
+
+    // VIRTUAL Update function
+    public virtual void Update(GameTime gameTime) {
+
+    }
+
+
+    // VIRTUAL Draw function
+    public virtual void Draw(GameTime gameTime) {
+
+    }
+
+
+  }
+
+}
