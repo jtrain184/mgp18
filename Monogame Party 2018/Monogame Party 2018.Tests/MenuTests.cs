5b80219135beacab5de9f99f19d5132c1d7a4219
Phil Jarrett <pejarrett@gmail.com>
2018-07-14 19:00:14 -0400

    Unit test attempt

---
+++ Monogame Party 2018/Monogame Party 2018.Tests/MenuTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Monogame_Party_2018.Tests
+{
+    public class MenuTests
+    {
+        [Fact]
+        public void Values_ShouldUpdate()
+        {
+            // Arrange
+            //MonogameParty testGame = new MonogameParty();
+            //GameStateManager testManager = testGame.manager;
+            // State mainMenu = new S_MainMenu(testManager, 0, 0);
+            //testGame.Run();
+            // Act
+
+
+            // Assert
+
+            Assert.Equal(5, 5);
+        }
+    }
+}
