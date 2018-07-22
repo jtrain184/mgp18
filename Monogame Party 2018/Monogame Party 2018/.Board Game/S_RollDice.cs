42b66224b62769d9dc29763212c46258c09cf48a
CBugsch <bugsch.christopher@gmail.com>
2018-07-22 15:31:04 -0700

    Added code to have computer players roll their own dice

--- Monogame Party 2018/Monogame Party 2018/.Board Game/S_RollDice.cs
+++ Monogame Party 2018/Monogame Party 2018/.Board Game/S_RollDice.cs
@@ -16,6 +16,8 @@ namespace Monogame_Party_2018
         bool isRolling = true;
         public int rollTime;
         public const int ROLL_SPEED = 20;    // Roll Speed = How many times you want it to change per second
+        public int compRollBegin;
+        public int compRollEnd;
 
         // Constructor
         public S_RollDice(GameStateManager creator, float xPos, float yPos) : base(creator, xPos, yPos)
@@ -23,6 +25,9 @@ namespace Monogame_Party_2018
             currPlayer = parentManager.round.currPlayer;
             rollTime = 60 / ROLL_SPEED; // Start rolling right away
             parentManager.boardGame.testDice.diceRoll = 1;  // start dice at 1;
+
+            compRollBegin = 0;
+            compRollEnd = creator.random.Next(ROLL_SPEED, ROLL_SPEED * 3); // have computer 'hit the dice' after 1 - 3 secs
         }
 
         // Update:
@@ -30,42 +35,38 @@ namespace Monogame_Party_2018
         {
             base.Update(gameTime, ks);
 
-            if (rollTime == 60 / ROLL_SPEED && isRolling) // Update twice per second
+            if (rollTime == 60 / ROLL_SPEED && isRolling) // update according to roll speed
             {
-                //isRolling = true;
+                
                 rollTime = 0;
                 parentManager.boardGame.testDice.Update(gameTime, ks);
+
+                // if comp is rolling 
+                if (!currPlayer.isHuman)
+                    compRollBegin++;
             }
             else
             {
-                //isRolling = false;
+                
                 rollTime++;
             }
 
 
-            /* if (isRolling)
-             {
 
-                 // testDice update
-                 parentManager.boardGame.testDice.Update(gameTime, ks);
-             }
-             */
-
-            if (km.ActionPressed(KeyboardManager.action.select, KeyboardManager.playerIndex.one))
+            // If human wait till select is pressed. If computer, wait till comp roll ends
+            if ((km.ActionPressed(KeyboardManager.action.select, KeyboardManager.playerIndex.one) && currPlayer.isHuman == true) || compRollBegin == compRollEnd)
             {
                 isRolling = false;
                 Console.WriteLine(currPlayer.type + " has rolled a " + parentManager.boardGame.testDice.diceRoll.ToString());
 
-                // DEBUG: Paused game here to make sure dice roll number matches what is displayed
 
-             
-                
+
                 // Get player roll and reset dice
                 S_MovePlayer movePlayer = new S_MovePlayer(parentManager, 0, 0, parentManager.boardGame.testDice.diceRoll);
-                
+
                 parentManager.AddStateQueue(movePlayer);
                 this.flagForDeletion = true;
-                
+
 
             }
         }
